50eca0b145431c706b4556f3359754aa7aa58fb5
Shefaya <shefayaparwin@gmail.com>
2021-08-16 14:44:51 +0530

    changes in adherence to coding guidelines

--- ReactSkia/platform/linux/RuntimeEventBeat.cpp
+++ ReactSkia/platform/linux/RuntimeEventBeat.cpp
@@ -1,11 +1,10 @@
 /*
  * Copyright (c) Facebook, Inc. and its affiliates.
+ * Copyright (C) 1994-2021 OpenTV, Inc. and Nagravision S.A.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
-#include <thread>
 
 #include "RuntimeEventBeat.h"
 #include "ReactSkia/utils/RnsLog.h"
@@ -20,12 +19,12 @@ namespace react {
   RNS implementation is not based on runloop*/
 RunLoopObserver::Activity activities{RunLoopObserver::Activity::BeforeWaiting};
 RuntimeEventBeat::RuntimeEventBeat(RunLoopObserver::WeakOwner const &owner)
-  : RunLoopObserver(activities, owner),baetThread_("BeatThread"),activities_{activities} {
-    baetThread_.getEventBase()->runInEventBaseThread(std::bind(&RuntimeEventBeat::beat,this));
+  : RunLoopObserver(activities, owner),beatThread_("BeatThread"),activities_{activities} {
+    beatThread_.getEventBase()->runInEventBaseThread(std::bind(&RuntimeEventBeat::beat,this));
 }
 
 RuntimeEventBeat::~RuntimeEventBeat() {
-  baetThread_.getEventBase()->terminateLoopSoon();
+  beatThread_.getEventBase()->terminateLoopSoon();
 }
 
 void RuntimeEventBeat::startObserving() const noexcept
@@ -36,7 +35,7 @@ void RuntimeEventBeat::startObserving() const noexcept
 
 void RuntimeEventBeat::beat(){
   this->activityDidChange(activities_);
-  baetThread_.getEventBase()->scheduleAt(std::bind(&RuntimeEventBeat::beat,this), \
+  beatThread_.getEventBase()->scheduleAt(std::bind(&RuntimeEventBeat::beat,this), \
            std::chrono::steady_clock::now() + std::chrono::milliseconds(BEAT_INTERVAL));
 }
 
@@ -49,7 +48,7 @@ void RuntimeEventBeat::stopObserving() const noexcept
 
 bool RuntimeEventBeat::isOnRunLoopThread() const noexcept
 {
-  return (baetThread_.getThreadId() == std::thread::id());
+  return (beatThread_.getThreadId() == std::thread::id());
 }
 
 } // namespace react
--- ReactSkia/platform/linux/RuntimeEventBeat.h
+++ ReactSkia/platform/linux/RuntimeEventBeat.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) Facebook, Inc. and its affiliates.
+ * Copyright (C) 1994-2021 OpenTV, Inc. and Nagravision S.A.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
@@ -25,7 +26,7 @@ class RuntimeEventBeat : public RunLoopObserver{
   void startObserving() const noexcept override;
   void stopObserving() const noexcept override;
   void beat();
-  folly::ScopedEventBaseThread baetThread_;
+  folly::ScopedEventBaseThread beatThread_;
   RunLoopObserver::Activity activities_;
 };
 
