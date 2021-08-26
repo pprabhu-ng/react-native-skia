a1a41891f2bbf372f5d3b9bcefc2e40b7be7aabe
root <chatrapathi06@gmail.com>
2021-08-26 11:25:30 +0530

    Key mapping for alpha and symbolic keys

--- packages/react-native-skia/modules/tvEventHandler/tvEventHandler.js
+++ packages/react-native-skia/modules/tvEventHandler/tvEventHandler.js
@@ -20,6 +20,7 @@ class SimpleViewApp extends React.Component {
     console.log('APP: TVEventHandler ',this._tvEventHandler);
     this._tvEventHandler.enable(this, function(cmp, evt) {
       console.log('APP: TV Key event received: ', evt);
+      console.log('APP: TV Key event received:  [evt.eventType]',  evt.eventType);
 
       if (evt && evt.eventType === 'right') {
         that.setState({bgColor: '#FF0000'})
