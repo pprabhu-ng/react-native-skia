b3d3f1373044d6be574fef56d294af75b09901de
pprabhu-ng <pprabhu.wns@gmail.com>
2022-01-17 14:03:40 +0530

    updated demo app with scrollview

---
+++ packages/react-native-skia/components/scrollview/demo2/contentData.js
@@ -0,0 +1,76 @@
+const imagePath = [
+   require('./images/poster1.jpg'),
+   require('./images/poster2.jpg'),
+   require('./images/poster3.jpg'),
+   require('./images/poster4.jpg'),
+   require('./images/poster5.jpg'),
+   require('./images/poster6.jpg'),
+   require('./images/poster7.jpg'),
+   require('./images/poster8.jpg'),
+]
+
+const title = [
+   "Frozen II",
+   "Bodied",
+   "Black Panther",
+   "The Lion King",
+   "Joker",
+   "The Addams Family",
+   "Accelaration",
+   "Rambo: Last Blood",
+]
+
+const genre = [
+   "Musical Comedy",
+   "Comedy",
+   "Action",
+   "Adventure",
+   "Crime drama",
+   "Comedy",
+   "Action",
+   "Action",
+]
+
+const duration = [
+   "1h  43m",
+   "2h  01m",
+   "2h  02m",
+   "1h  58m",
+   "2h  02m",
+   "1h  27m",
+   "1h  25m",
+   "1h  29m",
+]
+
+const year = [
+   "2019",
+   "2017",
+   "2018",
+   "2019",
+   "2019",
+   "2019",
+   "2019",
+   "2019",
+]
+
+const description = [
+   "Elsa the Snow Queen has an extraordinary gift -- the power to create ice and snow. But no matter how happy she is to be surrounded by the people of Arendelle, Elsa finds herself strangely unsettled.",
+
+   "A progressive graduate student finds success and sparks outrage when his interest in battle rap as a thesis subject turns into a competitive obsession.",
+
+   "After the death of his father, T'Challa returns home to the African nation of Wakanda to take his rightful place as king. When a powerful enemy suddenly reappears....",
+
+   "Betrayed and exiled from his kingdom, lion cub Simba must figure out how to grow up and take back his royal destiny on the plains of the African savanna.",
+
+   "Isolated, bullied and disregarded by society, failed comedian Arthur Fleck begins a slow descent into madness as he transforms into the criminal mastermind known as the Joker.",
+
+   "Members of the mysterious and spooky Addams family -- Gomez, Morticia, Pugsley, Wednesday, Uncle Fester and Grandma -- encounter a shady TV personality who despises their eerie hilltop mansion.",
+
+   "When an evil crime lord is double-crossed by his most trusted operative, he kidnaps her young son.",
+
+   "Vietnam War veteran John Rambo tries to find some semblance of peace by raising horses on a ranch in Arizona. He's also developed a special familial bond with a woman named Maria and her teenage   granddaughter Gabriela.",
+
+   "A new king rises in the north; a Khaleesi finds new hope in the season finale."
+]
+
+
