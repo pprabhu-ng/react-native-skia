b4c85bd6e3c3c7f25cbe840ff2702f78a79b42b1
pprabhu-ng <pprabhu.wns@gmail.com>
2022-04-01 18:29:10 +0530

    update test app

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,24 +1,9 @@
 describe('Example', () => {
-  beforeAll(async () => {
+  it('Launch SimpleViewApp', async () => {
+    //await expect(element(by.id('welcome'))).toBeVisible();
+    console.log("test - launch app");
     await device.launchApp();
+    await new Promise(resolve => setTimeout(resolve, 60000)); 	  
   });
 
-  beforeEach(async () => {
-    await device.reloadReactNative();
-  });
-/*
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
-  });
-
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
-  });
-  */
 });
